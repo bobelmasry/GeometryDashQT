6d7409d9ffbfac5eb69921fe550e37038699be5f
bobelmasry <alyhassan2254@gmail.com>
2024-05-02 11:03:45 +0300

    initial support for item shop database

--- itemshopwindow.cpp
+++ itemshopwindow.cpp
@@ -1,11 +1,35 @@
 #include "itemshopwindow.h"
 #include "ui_itemshopwindow.h"
 #include "mainwindow.h"
+#include <QFile>
+#include <QStringList>
+#include <QDebug>
 
 itemShopWindow::itemShopWindow(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::itemShopWindow)
 {
+    QFile file("D://python_projects//other shit//CS//GeometryDashQT//images//skinData.csv");
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << file.errorString();
+    }
+    std::vector<std::vector<QString>> data;
+
+    while (!file.atEnd()) {
+        QString line = file.readLine();
+        QStringList fields = line.split(',');
+
+        std::vector<QString> row;
+        for (const QString& field : fields) {
+            row.push_back(field);
+        }
+
+        data.push_back(row);
+    }
+
+
+    qDebug() << data;
+
     ui->setupUi(this);
 }
 
