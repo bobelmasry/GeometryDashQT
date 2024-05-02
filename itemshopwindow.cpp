9a1d1ba8b2d890d50dae787aa46685336adbe857
bobelmasry <alyhassan2254@gmail.com>
2024-05-02 11:26:53 +0300

    more support though null pixmap

--- itemshopwindow.cpp
+++ itemshopwindow.cpp
@@ -4,35 +4,55 @@
 #include <QFile>
 #include <QStringList>
 #include <QDebug>
+#include <QLabel>
+#include <QTextEdit>
+
 
 itemShopWindow::itemShopWindow(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::itemShopWindow)
 {
     QFile file("D://python_projects//other shit//CS//GeometryDashQT//images//skinData.csv");
-    if (!file.open(QIODevice::ReadOnly)) {
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qDebug() << file.errorString();
+        return;
     }
-    std::vector<std::vector<QString>> data;
 
-    while (!file.atEnd()) {
-        QString line = file.readLine();
+    QTextStream in(&file);
+    QString line = in.readLine(); // read and discard the first line
+    while (!in.atEnd()) {
+        QString line = in.readLine();
         QStringList fields = line.split(',');
 
-        std::vector<QString> row;
-        for (const QString& field : fields) {
-            row.push_back(field);
+        if (fields.size() < 5) {
+            qDebug() << "Invalid data format";
+            continue;
         }
 
-        data.push_back(row);
+        // extracting fields
+        QString skinName = fields[0];
+        QString price = fields[1];
+        QString isOwned = fields[2];
+        QString isEquipped = fields[3];
+        QString imagePath = fields[4].trimmed(); // to remove leading and trailing whitespace
+
+        // displaying image
+        QLabel *imageLabel = new QLabel;
+        imagePath = imagePath.replace("\"", "");
+        qDebug() << imagePath << '\n';
+        QPixmap pixmap(imagePath);
+        imageLabel->setPixmap(pixmap.scaled(100, 100));
+
+        // displaying details
+        QTextEdit *detailsTextEdit = new QTextEdit;
+        detailsTextEdit->setReadOnly(true);
+        QString details = QString("Skin Name: %1\nPrice: %2\nOwned: %3\nEquipped: %4").arg(skinName, price, isOwned, isEquipped);
+        detailsTextEdit->setPlainText(details);
     }
 
-
-    qDebug() << data;
-
+    file.close();
     ui->setupUi(this);
 }
-
 itemShopWindow::~itemShopWindow()
 {
     delete ui;
