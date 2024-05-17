e3975800b7c642a085d87eac9f25fa164e329ffd
omar-rabeh-18 <omar_galal@aucegypt.edu>
2024-05-18 02:30:55 +0300

    added comments to player.cpp

--- player.cpp
+++ player.cpp
@@ -13,6 +13,7 @@
 const qreal gravity = 15;
 const qreal jumpVelocity = -85;
 
+// constructor
 Player::Player(QGraphicsScene *scene)
     : QGraphicsPixmapItem()
     , health(1)
@@ -46,18 +47,17 @@ Player::Player(QGraphicsScene *scene)
 
     file.close();
 
-    // Set the pixmap using the image path of the equipped skin
+    // set the pixmap using the image path of the equipped skin
     qDebug() << equippedSkinImagePath;
     if (!equippedSkinImagePath.isEmpty()) {
         QPixmap pixmap(equippedSkinImagePath);
         if (!pixmap.isNull()) {
             setPixmap(pixmap.scaled(75, 75));
         } else {
-            setPixmap(QPixmap(":/images/square.png").scaled(75, 75)); // Fallback image if the pixmap is null
+            setPixmap(QPixmap(":/images/square.png").scaled(75, 75)); // fallback image if the pixmap is null
         }
     } else {
-        setPixmap(QPixmap(":/images/square.png").scaled(75, 75)); // Fallback image if no skin is equipped
-        // qDebug() << "No skin is currently equipped.";
+        setPixmap(QPixmap(":/images/square.png").scaled(75, 75)); // fallback image if no skin is equipped
     }
     setFlag(QGraphicsItem::ItemIsFocusable);
     setFocus();
@@ -81,12 +81,14 @@ Player::Player(QGraphicsScene *scene)
     particleTimer->start(500);
 }
 
+// this function increases coins and displays the number of coins
 void Player::increase()
 {
     coins++;
     coinDisplay->setPlainText("Coins: " + QString::number(coins));
 }
 
+// this function allows the player to jump using the space bar key
 void Player::keyPressEvent(QKeyEvent *event)
 {
     if (event->key() == Qt::Key_Space && !in_air) {
@@ -97,11 +99,13 @@ void Player::keyPressEvent(QKeyEvent *event)
     }
 }
 
+// this function sets the variable in_air
 void Player::setInAir(bool inAir)
 {
     in_air = inAir;
 }
 
+// this function advances the player
 void Player::advance()
 {
     yVelocity += gravity;
@@ -125,59 +129,67 @@ void Player::advance()
     emit inAirChanged(in_air);
 }
 
+// this function creates an enemy
 void Player::createEnemy()
 {
     Enemy *enemy = new Enemy(*this);
     scene()->addItem(enemy);
 }
 
+
+// this function creates a coin
 void Player::createCoin()
 {
     coin *Coin = new coin();
     scene()->addItem(Coin);
 }
 
+// this function creates a platform
 void Player::createPlatform()
 {
     int randomX = QRandomGenerator::global()->bounded(600, 800 - pixmap().width());
 
-    // Make sure that the platform isn't colliding with any enemies on spawn
+    // make sure that the platform isn't colliding with any enemies on spawn
     QList<QGraphicsItem*> collidingItems;
     do {
-        Platform *block = new Platform(); // Create a new platform instance
-        block->setPos(randomX + 1300, 540); // Set the position of the platform
-        collidingItems = block->collidingItems(); // Check for collisions with enemies
+        Platform *block = new Platform(); // create a new platform instance
+        block->setPos(randomX + 1300, 540); // set the position of the platform
+        collidingItems = block->collidingItems(); // check for collisions with enemies
         if (!collidingItems.isEmpty()) {
             randomX = QRandomGenerator::global()->bounded(400, 600 - pixmap().width());
         }
         else {
-            scene()->addItem(block); // Add the platform to the scene if there are no collisions
+            scene()->addItem(block); // add the platform to the scene if there are no collisions
         }
-    } while (!collidingItems.isEmpty()); // Repeat until no collisions are detected
+    } while (!collidingItems.isEmpty()); // repeat until no collisions are detected
 }
 
-
+// this function checks whether the player is in the air
 bool Player::isInAir() const
 {
     return in_air;
 }
 
+// this function gets the y velocity
 qreal Player::getYVelocity() const
 {
     return yVelocity;
 }
 
+// this function set the yvelocity
 void Player::setYVelocity(qreal velocity)
 {
     yVelocity = velocity;
 }
 
+// this function resets the coins
 void Player::resetCoins()
 {
     coins = 0;
     coinDisplay->setPlainText("Coins: " + QString::number(coins));
 }
 
+// this function allows the player to rotate
 void Player::rotation()
 {
     static qreal currentRotation = 0;
@@ -200,11 +212,13 @@ void Player::rotation()
     }
 }
 
+// this function sets the position
 void Player::setPosition(qreal x, qreal y)
 {
     setPos(x, y);
 }
 
+// this function shows the attempts
 void Player::showAttempts()
 {
     QGraphicsTextItem *attemptsText = new QGraphicsTextItem(
@@ -218,29 +232,30 @@ void Player::showAttempts()
     attemptsText->setDefaultTextColor(Qt::white);
 
     int textWidth = attemptsText->boundingRect().width();
-    int playerX = this->x();                                        // Get player's x position
-    attemptsText->setPos(playerX - textWidth / 2, this->y() - 200); // Position above the player
+    int playerX = this->x();                                        // get player's x position
+    attemptsText->setPos(playerX - textWidth / 2, this->y() - 200); // position above the player
     scene()->addItem(attemptsText);
 
-    // Create a QTimer to start moving the text item after a delay
+    // create a QTimer to start moving the text item after a delay
     QTimer::singleShot(1000, [=]() {
-        // Create a QTimer to update the position every 30 milliseconds
+        // create a QTimer to update the position every 30 milliseconds
         QTimer *timer = new QTimer(this);
         connect(timer, &QTimer::timeout, [=]() {
-            attemptsText->moveBy(-15, 0); // Move the text item 15 pixels to the left
-            if (attemptsText->pos().x() + textWidth < 0) { // If the text item is out of scene
+            attemptsText->moveBy(-15, 0); // move the text item 15 pixels to the left
+            if (attemptsText->pos().x() + textWidth < 0) { // if the text item is out of scene
                 scene()->removeItem(attemptsText);
                 delete attemptsText;
-                timer->stop();        // Stop the timer
-                timer->deleteLater(); // Delete the timer
+                timer->stop();        // stop the timer
+                timer->deleteLater(); // delete the timer
             }
         });
-        timer->start(30); // Start the timer with a 30-millisecond interval
+        timer->start(30); // start the timer with a 30-millisecond interval
     });
 
     resetCoins();
 }
 
+// this function creates a trail of particles
 void Player::emitParticles()
 {
     qreal particleY = y()
