c06e4b1a1302b186d2976310755cf2c1d0bdf6f2
omar-rabeh-18 <omar_galal@aucegypt.edu>
2024-05-18 01:05:01 +0300

    added comments for coin.cpp

--- coin.cpp
+++ coin.cpp
@@ -10,14 +10,15 @@
 #include <typeinfo>
 #include <QStandardPaths>
 
-QList<coin*>coin::coins;
+QList<coin*>coin::coins; // stores coin objects
 
 coin::coin() {
 
-    coins.append(this);
+    coins.append(this); // adds the current coin object to the list of coins
 
-    setPixmap(QPixmap("://images/coin.png").scaled(100,100));
+    setPixmap(QPixmap("://images/coin.png").scaled(100,100)); // sets the pixmap for the coin
 
+    // generates a random position for the coin within a certain range
     int randomX = QRandomGenerator::global()->bounded(600, 800 - pixmap().width());
 
     // makes sure that coin isn't colliding with any enemies on spawn
@@ -30,27 +31,32 @@ coin::coin() {
         }
     } while (!colliding_items.isEmpty()); // repeats until no collisions are detected
 
+    // sets up a timer to move the coin
     QTimer *timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(move()));
     timer->start(30);
 }
 
+// this function updates the text file
 void coin::updateTextFile() {
+    // gets the path to the desktop directory
     QString desktopDir = QStandardPaths::writableLocation(QStandardPaths::DesktopLocation);
     QString filePath = desktopDir + "/data.txt";
 
+    // opens the file for reading and writing
     QFile file(filePath);
     if (file.open(QIODevice::ReadWrite | QIODevice::Text)) {
         qDebug() << "File opened successfully.";
         QTextStream in(&file);
-        int value = in.readLine().toInt();
+        int value = in.readLine().toInt(); // reads the current value from the file
         value++; // Increment the value
 
+        // writes the updated value back to the file
         file.seek(0);
         QTextStream out(&file);
         out << value;
 
-        file.close();
+        file.close(); // closes the file
         qDebug() << "File closed.";
     } else {
         qDebug() << "Failed to open file for reading and writing. Error:" << file.errorString();
@@ -59,9 +65,12 @@ void coin::updateTextFile() {
 
 
 void coin::move() {
-    setPos(x() - 15, y());
+    setPos(x() - 15, y()); // moves coin horizontally
+
+    //checks for collisions with other items
     QList<QGraphicsItem*> colliding_items = collidingItems();
 
+    // if the coin moves out of the scene, remove it from the scene and delete it
     if (x() + pixmap().width() < pixmap().width()) {
         scene()->removeItem(this);
         coins.removeOne(this);
@@ -69,6 +78,7 @@ void coin::move() {
         return;
     }
 
+    // check if the coin collides with the player
     for (int i = 0; i < colliding_items.size(); ++i) {
         if (typeid(*(colliding_items[i])) == typeid(Player)) {
             Player *player = dynamic_cast<Player*>(colliding_items[i]);
