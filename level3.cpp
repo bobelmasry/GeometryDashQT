8d8b0cb613dd677b000f0a99de13bf905fb1ac58
omar-rabeh-18 <omar_galal@aucegypt.edu>
2024-05-18 01:56:35 +0300

    added comments to level3.cpp

--- level3.cpp
+++ level3.cpp
@@ -10,108 +10,121 @@
 #include <QGraphicsPixmapItem>
 #include "level_base.h"
 
+// declare static member variables
+QGraphicsScene* level3::scene;
+QGraphicsView* level3::view;
+QTimer* level3::coin_timer;
+QTimer* level3::platform_timer;
+QTimer* level3::enemy_timer;
+QMediaPlayer* level3::level3_music = nullptr;
 
-
-QGraphicsScene*level3::scene;
-QGraphicsView*level3::view;
-QTimer*level3::coin_timer;
-QTimer*level3::platform_timer;
-QTimer*level3::enemy_timer;
-
-QMediaPlayer*level3::level3_music=nullptr;
-
+// constructor
 level3::level3()
 {
-    level_base::level=3;
+    // set the level to 3
+    level_base::level = 3;
+
+    // create a new graphics scene
     scene = new QGraphicsScene();
 
+    // create a new graphics view with the scene
     view = new QGraphicsView(scene);
 
-    set_level(scene,view);
+    // set up the level with the scene and view
+    set_level(scene, view);
 
+    // play the background music
     play_music();
 
+    // create and start the elapsed timer
     level_base::elapsed_timer_creator(scene);
 
-    Player *player = new Player(scene);
+    // create a new player object and add it to the scene
+    Player* player = new Player(scene);
     scene->addItem(player);
-    player->setPos(300,400);
+
+    // set the player's position
+    player->setPos(300, 400);
+
+    // set the view to focus on the player
     view->setFocus();
 
-    QTimer *player_timer = new QTimer();
+    // create a timer to advance the player's state
+    QTimer* player_timer = new QTimer();
     QObject::connect(player_timer, SIGNAL(timeout()), player, SLOT(advance()));
     player_timer->start(50);
 
+    // create timers for generating enemies, coins, and platforms
     enemy_timer = new QTimer();
-    coin_timer= new QTimer();
-    platform_timer= new QTimer();
+    coin_timer = new QTimer();
+    platform_timer = new QTimer();
 
+    // connect timers to the respective player functions
     QObject::connect(enemy_timer, SIGNAL(timeout()), player, SLOT(createEnemy()));
     QObject::connect(coin_timer, SIGNAL(timeout()), player, SLOT(createCoin()));
-    //QObject::connect(platform_timer, SIGNAL(timeout()), player, SLOT(createPlatform()));
 
+    // start the timers with specified intervals
     enemy_timer->start(700);
-    //platform_timer->start(1200);
     coin_timer->start(1000);
 
+    // show the view in full screen mode
     view->showFullScreen();
-
-
 }
 
-void level3::set_level(QGraphicsScene*scene,QGraphicsView*view)
+// function to set up the level
+void level3::set_level(QGraphicsScene* scene, QGraphicsView* view)
 {
+    // set the scene rectangle size
     scene->setSceneRect(0, 0, 1560, 870);
 
-
+    // disable scroll bars
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+    // set the background image
     QPixmap backgroundImage(":/images/level3_background.png");
     backgroundImage = backgroundImage.scaled(scene->width(), scene->height());
-    QGraphicsPixmapItem *background = new QGraphicsPixmapItem(backgroundImage);
+    QGraphicsPixmapItem* background = new QGraphicsPixmapItem(backgroundImage);
     scene->addItem(background);
     background->setPos(0, 0);
 
+    // set the floor image
     QPixmap level_floor_image(":/images/level3_floor.png");
-    level_floor_image=level_floor_image.scaled(scene->width(),400);
-    QGraphicsPixmapItem*level1_floor=new QGraphicsPixmapItem(level_floor_image);
-    level1_floor->setPos(0,scene->height()-200);
+    level_floor_image = level_floor_image.scaled(scene->width(), 400);
+    QGraphicsPixmapItem* level1_floor = new QGraphicsPixmapItem(level_floor_image);
+    level1_floor->setPos(0, scene->height() - 200);
     scene->addItem(level1_floor);
-
-
 }
 
+// function to play background music
 void level3::play_music()
 {
-    //code to play sound when clicking on level
+    // code to play sound when clicking on level
     start_level_audio->setSource(QUrl("qrc:/Sound/start_level_audio.mp3"));
     start_level_audio->setAudioOutput(start_level);
     start_level->setVolume(50);
     start_level_audio->play();
 
-    // Initialize level1_music
+    // initialize level3_music
     level3_music = new QMediaPlayer();
     level3_music->setSource(QUrl("qrc:/Sound/level3_music.mp3"));
     level3_music->setAudioOutput(jumper);
     jumper->setVolume(50);
 
-    // Delay the level1_music audio
+    // delay the level3_music audio
     QTimer::singleShot(1000, this, []() {
         if (level3_music) {
             level3_music->play();
         }
     });
-
 }
 
-
-
+// function to handle level completion
 void level3::level_complete()
 {
     qDebug() << "entered level complete";
 
-    // Stop the timers
+    // stop the timers if they are active
     if (coin_timer && coin_timer->isActive())
         coin_timer->stop();
     if (enemy_timer && enemy_timer->isActive())
@@ -121,24 +134,25 @@ void level3::level_complete()
 
     qDebug() << "stopped timers";
 
-    // Ensure scene and view are valid
+    // ensure scene and view are valid
     if (!scene || !view) {
         qDebug() << "scene or view is null!";
         return;
     }
 
-    // Capture necessary pointers for lambda functions
-    QGraphicsScene *local_scene = scene;
-    QGraphicsView *local_view = view;
+    // capture necessary pointers for lambda functions
+    QGraphicsScene* local_scene = scene;
+    QGraphicsView* local_view = view;
 
-    // Using a single-shot timer to delay actions by 3 seconds
+    // use a single-shot timer to delay actions by 3 seconds
     QTimer::singleShot(3000, [local_scene, local_view]() {
         if (!local_scene || !local_view) {
             qDebug() << "local_scene or local_view is null in singleShot!";
             return;
         }
 
-        QGraphicsTextItem *gameOverText = new QGraphicsTextItem("Level Complete!");
+        // display "level complete" text in the center of the screen
+        QGraphicsTextItem* gameOverText = new QGraphicsTextItem("Level Complete!");
         QFont font;
         font.setPointSize(30);
         gameOverText->setFont(font);
@@ -147,7 +161,7 @@ void level3::level_complete()
                              (local_scene->height() - gameOverText->boundingRect().height()) / 2);
         local_scene->addItem(gameOverText);
 
-        // Close the window after another 3 seconds
+        // close the window after another 3 seconds
         QTimer::singleShot(3000, [local_view]() {
             if (!local_view) {
                 qDebug() << "local_view is null in second singleShot!";
@@ -156,7 +170,8 @@ void level3::level_complete()
 
             local_view->close();
 
-            MainWindow *windowObj = new MainWindow();
+            // show the main window
+            MainWindow* windowObj = new MainWindow();
             windowObj->show();
             if (level3::level3_music)
                 level3::level3_music->stop();
@@ -164,4 +179,5 @@ void level3::level_complete()
     });
 }
 
+// destructor
 level3::~level3() {}
